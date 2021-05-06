bd028dd0ff4b54bf40379eef775695242f7422ee
Dima <warreaver123@gmail.com>
2021-05-06 16:08:04 +0300

    row

--- src/js/script.js
+++ src/js/script.js
@@ -13,5 +13,17 @@ jQuery(document).ready(function($) {
     $(".about__slider .about__slide").on("click", function() {
         const index = $(this).attr("data-slick-index");
         $(".about__slider").slick("slickGoTo", index);
-      });
+    });
+
+    // $('.container').each(function(){
+    //     var highestBox = 0;
+    //     $('.col-md-5 ', this).each(function(){
+    //       if($(this).height() > highestBox) {
+    //         highestBox = $(this).height();
+    //       }
+    //     });
+    //     $('.col-md-5 ',this).height(highestBox);
+    // });
+    
+
 })
